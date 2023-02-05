d4af57d6e79331b2e705c633b17f9e82b921f051
mjover <mjover@uncc.edu>
2023-02-04 23:31:08 -0500

    Adding prefabs for design team to build levels

--- Assets/Systems/UI/CrosshairFollowCursor.cs
+++ Assets/Systems/UI/CrosshairFollowCursor.cs
@@ -6,6 +6,12 @@ public class CrosshairFollowCursor : MonoBehaviour
     public Image crosshairImage;
     RectTransform _rectTransform;
 
-    private void Start() => _rectTransform = crosshairImage.GetComponent<RectTransform>();
+    void OnEnable() => Cursor.visible = false;
+    void OnDisable() => Cursor.visible = true;
+    
+    private void Start() 
+    {
+        _rectTransform = crosshairImage.GetComponent<RectTransform>();
+    }
     private void Update() => _rectTransform.position = InputListener.AbosluteMousePosition;
 }
